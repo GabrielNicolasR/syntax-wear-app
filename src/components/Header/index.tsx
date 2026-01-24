43348728881949158add042fab9d1ac48210167a
Gabriel Nicolas <gabrielrochasa@gmail.com>
2026-01-24 15:14:58 -0300

    feat: configuração das rotas da aplicação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,14 @@ import Logo from "@/assets/images/logo.png";
 import IconUser from "@/assets/images/icon-user.png";
 import IconAbout from "@/assets/images/icon-about.png";
 import IconCart from "@/assets/images/icon-cart.png";
+import { Link } from "@tanstack/react-router";
 
 export const Header = () => {
   return (
     <div className="relative">
       <header className="fixed top-10 left-0 right-0 z-10 mx-10">
         <div className="bg-white max-w-330 mx-auto flex justify-between items-center py-3 px-7 rounded-2xl mt-5">
-          <img src={Logo} alt="Logo SyntaxWear" className="w-32 md:w-36"/>
+          <img src={Logo} alt="Logo SyntaxWear" className="w-32 md:w-36" />
 
           <nav className="hidden lg:block">
             <ul className="flex gap-10">
@@ -33,13 +34,19 @@ export const Header = () => {
                 <a href="#">Sobre</a>
               </li>
               <li>
-                <a href="#"><img src={IconUser} alt="Ícone de login" /></a>
+                <Link to="/sign-in">
+                  <img src={IconUser} alt="Ícone de login" />
+                </Link>
               </li>
               <li>
-                <a href="#"><img src={IconAbout} alt="Ícone de sobre" /></a>
+                <a href="#">
+                  <img src={IconAbout} alt="Ícone de sobre" />
+                </a>
               </li>
               <li>
-                <a href="#"><img src={IconCart} alt="Ícone de carrinho" /></a>
+                <a href="#">
+                  <img src={IconCart} alt="Ícone de carrinho" />
+                </a>
               </li>
             </ul>
           </nav>
